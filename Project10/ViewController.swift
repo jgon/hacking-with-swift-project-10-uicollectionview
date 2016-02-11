0d10dcbdedea82fc47920f508c37dac87b9cc3d9
JackG <jacques.gonzalez@me.com>
2016-02-11 19:34:19 +0100

    Implemented collection view data source methods.

--- Project10/ViewController.swift
+++ Project10/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     @IBOutlet weak var collectionView: UICollectionView!
 
@@ -22,6 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Person", forIndexPath: indexPath) as! PersonCollectionViewCell
+        return cell
+    }
 
 }
 
--- README.md
+++ README.md
@@ -8,3 +8,6 @@
 - iPad
 
 ## Topics covered
+
+- By default all protocols methods are [required](https://developer.apple.com/library/ios/documentation/Cocoa/Conceptual/ProgrammingWithObjectiveC/WorkingwithProtocols/WorkingwithProtocols.html).
+Optional protocols methods are defined using the ```optional``` keyword.
