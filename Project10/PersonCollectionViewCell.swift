98e498f13b1ed913ffd59f94108789a2388a11aa
JackG <jacques.gonzalez@me.com>
2016-02-11 18:03:49 +0100

    Configured storyboard and collection view cell.

---
+++ Project10/PersonCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCollectionViewCell.swift
+//  Project10
+//
+//  Created by Jacques on 11/02/16.
+//  Copyright © 2016 J4SOFT. All rights reserved.
+//
+
+import UIKit
+
+class PersonCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Hacking with Swift - Project 10 - UICollectionViews
+
+## Setup
+
+- Xcode 7.2.1
+- Swift 2.0
+- iOS 9.2
+- iPad
+
+## Topics covered
